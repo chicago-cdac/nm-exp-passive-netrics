7bdbe1002fd7141be89390b782163bcdc8c3649e
Tarun <tarun@Taruns-MacBook-Pro-2.local>
2021-04-29 13:04:03 -0500

    passive consumption

--- cmd/netricsd/main.go
+++ cmd/netricsd/main.go
@@ -15,12 +15,15 @@ import (
 	"strconv"
 )
 
+// work with offline capture 
+
 var (
 	signal = flag.String("s", "", `Send signal to the daemon:
   stop — shutdown`)
 	input = flag.String("r", "", "Proccess offline pcap as input.")
 	device = flag.String("i", "en0", "Interface to capture")
 	snapshot_len = flag.String("slen", "96", "snapshot length")
+	filter = flag.String("f", "", "capture filter")
 	ethLayer layers.Ethernet
 	ipLayer  layers.IPv4
 	ip6Layer layers.IPv6 // TODO: Add it for ipv6 traffc
@@ -36,39 +39,58 @@ var (
 
 const logFileName = "netricsd.log"
 const pidFileName = "netricsd.pid"
+const dataLogFilePref = "nm_passive_consumption"
 
 // saves the data to a file
 func flush_data(timestamp int64, data [60]int) {
-	f, err := os.OpenFile("tmp.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	//new file for every minute 
+	//define a constant for the file name template  
+	fname := fmt.Sprintf("%s_%d.csv", dataLogFilePref, timestamp)
+	f, err := os.OpenFile(fname, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0640)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error opening log file: %s", err)
 	}
 	defer f.Close()
 
 	delim := ","
-	data_str := fmt.Sprint(timestamp) + ":" + strings.Trim(strings.Join(strings.Fields(fmt.Sprint(data)), delim), "[]")
+	data_str := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(data)), delim), "[]")
+
 	_, err2 := f.WriteString(data_str + "\n")
 	if err2 != nil {
-		//log
+		log.Fatalln("Error writing to log file: %s", err)
 	}
-	fmt.Println("written")
+
+	fmt.Println("Data written for %d", timestamp)
 }
 
 
 
 func main() {
-	var pcapoffline string
+	//var pcapoffline string
 	flag.Parse()
+	snapshot_len_int, err :=  strconv.Atoi(*snapshot_len)
+	//fmt.Printf("%s %s", *device, *input)
 
 	if len(*device) > 0 {
-	//pcap open live
+	    fmt.Println("entered here")
+	    handle, err = pcap.OpenLive(*device, int32(snapshot_len_int), promiscuous, timeout)
+	    if err != nil {log.Fatal(err) }
+
+	    if *filter != "" {
+		err = handle.SetBPFFilter(*filter)
+		if err != nil { log.Fatal(err) }
+	    }
 	} else if len(*input) > 0 {
-		if strings.HasSuffix(*input, ".pcap") {
-			pcapoffline = *input
-			fmt.Println(pcapoffline)
-		}
+	    log.Printf("opening pcap file %s", *input) 
+	    handle, err = pcap.OpenOffline(*input)
+	    if err != nil { log.Fatal(err) }
+	    //defer handle.Close()
 	} else {
+	    fmt.Println("entered here too")
+	    log.Printf("%s %s", *input, *device)
+	    log.Fatal("Specify either device or pcap file %s %s", *input, *device)
 	}
+
 	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGTERM, termHandler)
 
 	cntxt := &daemon.Context{
@@ -99,13 +121,13 @@ func main() {
 		return
 	}
 	defer cntxt.Release()
+	fmt.Println("starting daemon")
+	log.Println("- - - - - - - - - - - - - - -")
+	log.Println("daemon started")
 
-	//log.Println("- - - - - - - - - - - - - - -")
-	//log.Println("daemon started")
+	//setupLog()
 
-	setupLog()
-
-	go worker()
+	go worker(handle)
 
 	err = daemon.ServeSignals()
 	if err != nil {
@@ -138,57 +160,59 @@ var (
 	done = make(chan struct{})
 )
 
-func worker() {
-
-    snapshot_len_int, err :=  strconv.Atoi(*snapshot_len)
-    handle, err = pcap.OpenLive(*device, int32(snapshot_len_int), promiscuous, timeout)
-    if err != nil {log.Fatal(err) }
+func worker(handle *pcap.Handle) {
     defer handle.Close()
-
-    // Use the handle as a packet source to process all packets
+   // Use the handle as a packet source to process all packets
     packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
     currentMin := int64(-1)
     var newTime, newMin, newSec int64
 
     var per_minute_log [60] int
-    for packet := range packetSource.Packets() {
+    LOOP:
+	    for packet := range packetSource.Packets() {
 
-	parser := gopacket.NewDecodingLayerParser(
-		layers.LayerTypeEthernet,
-		&ethLayer,
-		&ipLayer,
-		&ip6Layer,
-	)
+		parser := gopacket.NewDecodingLayerParser(
+			layers.LayerTypeEthernet,
+			&ethLayer,
+			&ipLayer,
+			&ip6Layer,
+		)
 
-	foundLayerTypes := []gopacket.LayerType{}
+		foundLayerTypes := []gopacket.LayerType{}
 
-	err := parser.DecodeLayers(packet.Data(), &foundLayerTypes)
-	if err != nil {
-		//fmt.Println("Trouble decoding layers: ", err)
-	}
-
-	for _, layerType := range foundLayerTypes {
-		if layerType == layers.LayerTypeIPv4 {
-			//fmt.Println(packet.Metadata().Timestamp.Unix(), packet.Metadata().Timestamp)
-			//fmt.Println("IPv4: ", ipLayer.SrcIP, "->", ipLayer.DstIP, "length:", ipLayer.Length)
-		//filtering logic here. Skip all the measurement traffic from the pi
+		err := parser.DecodeLayers(packet.Data(), &foundLayerTypes)
+		if err != nil {
+			//fmt.Println("Trouble decoding layers: ", err)
 		}
-	}
 
-	newTime = packet.Metadata().Timestamp.Unix()
-	newMin = 60*int64(newTime / 60)
-	newSec = newTime % 60
-	if currentMin != newMin {
-		if currentMin != -1 {
-			flush_data(currentMin, per_minute_log)
+		for _, layerType := range foundLayerTypes {
+			if layerType == layers.LayerTypeIPv4 {
+			//filtering logic here. Skip all the measurement traffic from the pi
+			}
 		}
-		currentMin = newMin
-		per_minute_log = [60] int{}
-	}
 
-	per_minute_log[newSec] += int(ipLayer.Length)
-    }
+		newTime = packet.Metadata().Timestamp.Unix()
+		newMin = 60*int64(newTime / 60)
+		newSec = newTime % 60
+		if currentMin != newMin {
+			if currentMin != -1 {
+				flush_data(currentMin, per_minute_log)
+			}
+			currentMin = newMin
+			per_minute_log = [60] int{}
+		}
 
+		per_minute_log[newSec] += int(ipLayer.Length)
+
+		select {
+			case <-stop:
+				break LOOP
+			default:
+		}
+	    }
+	//flush remaining data
+	flush_data(currentMin, per_minute_log)
+	log.Printf("%s: Exiting daemon", time.Now().String())
 	done <- struct{}{}
 }
 
